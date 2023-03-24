e3588f88cdbfad5817b13c2ab08259b883738b51
MAULIK RAJENDRA PATEL <60727880+konregon@users.noreply.github.com>
2023-03-24 17:41:56 +0530

    Update app.js

--- FreeCodeCamp/Landing page/assets/js/app.js
+++ FreeCodeCamp/Landing page/assets/js/app.js
@@ -22,3 +22,11 @@ function openMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
+
+const navLink = document.querySelectorAll("#nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
